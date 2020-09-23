72fc8e0e9b89a437de45dc472edac25855b73475
Shahzad Haider <71204683+shahzadhaider7@users.noreply.github.com>
2020-09-23 22:27:32 +0500

    Add files via upload

---
+++ data_preprocessing_template.py
@@ -0,0 +1,27 @@
+# Data Preprocessing Template
+# You can make changes in it according to your own needs and use it
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('dataset.csv')
+X = dataset.iloc[,].values
+y = dataset.iloc[,].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
+
+#### Sci-Kit Learn makes everything easy ####
